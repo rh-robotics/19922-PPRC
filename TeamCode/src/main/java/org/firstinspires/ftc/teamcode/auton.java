50c71f6ca1146e28cc7349259b79002453d9dbdc
DragonDev07 <teowelton@gmail.com>
2022-10-24 22:01:13 -0600

    Build Basic state machine and implement CV into main auton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton.java
@@ -1,18 +1,88 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.HWC;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
 @Autonomous
 public class auton extends LinearOpMode {
-  states
+  // TODO: Move state to bronto HWC
+  enumStates state = enumStates.SCANNING_FOR_SIGNAL;
+  ElapsedTime timer = new ElapsedTime();
+
+  // Variables for CV
+  // TODO: Move to bronto HWC
+  SleeveDetection sleeveDetection = new SleeveDetection();
+  OpenCvCamera camera;
+  String webcamName = "Webcam 1";
 
   @Override
   public void runOpMode() throws InterruptedException {
-    HWC robot = new HWC(hardwareMap, telemetry);
+    HWC bronto = new HWC(hardwareMap, telemetry);
 
+    // Tell driver it is initializing
     telemetry.addData("Status", "Initializing");
     telemetry.update();
 
+    // Run any initialization code necessary
+
+    // Tell driver ready and waiting for start
+    telemetry.addData("Status", "Initialized - Waiting for Start");
+    telemetry.addData("State", "Scanning for Signal");
+    telemetry.update();
+
+    // Vision Code Implement - Jack Might need to modify
+    int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+    camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, webcamName), cameraMonitorViewId);
+    sleeveDetection = new SleeveDetection();
+    camera.setPipeline(sleeveDetection);
+
+    camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+    {
+      @Override
+      public void onOpened()
+      {
+        camera.startStreaming(320,240, OpenCvCameraRotation.SIDEWAYS_LEFT);
+      }
+
+      @Override
+      public void onError(int errorCode) {}
+    });
+
+    while (!isStarted()) {
+      telemetry.addData("ROTATION: ", sleeveDetection.getPosition());
+      telemetry.update();
+    }
+
+    // Wait for Driver to press start
     waitForStart();
-    while(opModeIsActive()) {
+    while (opModeIsActive()) {
+      // State Machine
+      if (state == enumStates.MOVING_TO_POLE) {
+        telemetry.addData("State", "Moving to Pole");
+        telemetry.update();
+      } else if (state == enumStates.DELIVERING_CONE) {
+        telemetry.addData("State", "Delivering Cone");
+        telemetry.update();
+      } else if (state == enumStates.MOVING_TO_STACK) {
+        telemetry.addData("State", "Moving to Stack");
+        telemetry.update();
+      } else if (state == enumStates.PICKING_UP_CONE) {
+        telemetry.addData("State", "Picking up Cone");
+        telemetry.update();
+      } else if (state == enumStates.PARKING_NO_VALUE) {
+        telemetry.addData("State", "Parking no Value");
+        telemetry.update();
+      } else if (state == enumStates.PARKING_VALUE) {
+        telemetry.addData("State", "Parking with Value");
+        telemetry.update();
+      }
     }
   }
 }
