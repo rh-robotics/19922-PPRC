dd9ad3503d5c241a68019c5ba536bf0c5b629221
DragonDev07 <teowelton@gmail.com>
2022-12-04 21:00:16 -0700

    Auton arm movement with PID

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -32,7 +32,7 @@ public class DriveConstants {
      * from DriveVelocityPIDTuner.
      */
     public static final boolean RUN_USING_ENCODER = true;
-    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(15, 0, 10, 13.220977778303842);
+    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(15, 0, 0, 13.220977778303842);
 
     /*
      * These are physical constants that can be determined from your robot (including the track
